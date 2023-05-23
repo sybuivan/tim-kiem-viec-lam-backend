91f11dff204c37bf3d5365438b8403842cd18b8e
buivansy <your github email address>
2023-05-23 22:37:10 +0700

    fix config

--- src/configs/connectDb.ts
+++ src/configs/connectDb.ts
@@ -1,12 +1,23 @@
 import mysql from 'mysql2/promise';
 // // create the connection to database
 
+// const config = {
+//   db: {
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'db_tim-kiem-viec-lam',
+//   },
+//   listPerPage: 10,
+// };
+
 const config = {
   db: {
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'mysql-127952-0.cloudclusters.net',
+    user: 'admin',
+    password: 'vPgn2oDF',
     database: 'db_tim-kiem-viec-lam',
+    port: 19985,
   },
   listPerPage: 10,
 };
